b289646bb1dacd6c80acba74a2f2de68d9d14ec0
nereazj544 <nzjvirtual@gmail.com>
2023-12-05 12:50:19 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "examen_NereaZJ_PMDM"
+include(":app")
+ 
